c34d0d8cacd089de46c181a5be375f4b0c79a8ef
Gabriel Adewumi <codergab@gmail.com>
2019-10-15 19:40:37 +0100

    Removed ENV

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const DB_URI = 'mongodb://127.0.0.1:27017/food-fire' || process.env.MONGODB_URI;
+const DB_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/food-fire';
 mongoose.connect(DB_URI, {
 	useNewUrlParser: true,
 	useCreateIndex: true,
